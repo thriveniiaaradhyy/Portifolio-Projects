4e6868594982ab3213e831ab72451e1777f14118
Thriveni V <64365170+thriveniiaaradhyy@users.noreply.github.com>
2025-02-06 18:43:50 +0530

    Update and rename EDA SQLQuery.sql to Covid Portifolio Project.sql

--- Covid Portifolio Project.sql
+++ Covid Portifolio Project.sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration using SQL
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 select *
 from PortifolioProject..CovidDeaths
 order by 3,4;
@@ -6,13 +12,14 @@ select *
 from PortifolioProject..CovidVaccinations
 order by 3,4;
 
--- View data 
+-- Selecting Data columns that we are going to start our Exploration with
 
 select location, date, total_cases, new_cases, total_deaths, population
 from PortifolioProject..CovidDeaths
 order by 1,2;
 
--- total cases vs total deaths
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
 
 select location, date, total_cases, total_deaths, 
 (total_deaths/total_cases) * 100 as PercentPopulationDied
@@ -21,7 +28,8 @@ where location like '%indi%'
 order by 1,2;
 
 
--- Total cases vs population(% of the population affected with covid)
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid in your country
 
 select location, date, population, total_cases,
 (total_cases/population) * 100 as PercentPopulationInfected
@@ -29,7 +37,11 @@ from PortifolioProject..CovidDeaths
 where location like '%indi%'
 order by 1,2;
 
--- Countries with Highest infection rate compared to population
+-- Countries with the highest percentage of their population infected with COVID.
+/*
+The query finds the highest recorded infection count and the percentage of the population infected for each location, 
+then sorts the results in descending order of infection percentage.
+*/
 
 select location, population, max(total_cases) as HighestInfectionCount,
 max((total_cases/population) * 100) as PercentPopulationInfected
@@ -38,21 +50,24 @@ from PortifolioProject..CovidDeaths
 group by location, population
 order by PercentPopulationInfected desc;
 
--- Countries with Highest Death Count 
+-- Countries with the highest death count per capita.
 
 select location, max(cast(total_deaths as int)) as TotalDeathCount
 from PortifolioProject..CovidDeaths
 group by location
 order by TotalDeathCount desc;
 
--- Continent with Highest Death Count
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Countries with the highest death count per capita.
 
 select continent, max(cast(total_deaths as int)) as TotalDeathCount
 from PortifolioProject..CovidDeaths
 group by continent
 order by TotalDeathCount desc;
 
--- Global Numbers
+-- Global Numbers -> Total Cases, Total Deaths, Total Percentage of People Died
 
 select 
 --date, 
@@ -74,7 +89,7 @@ Join PortifolioProject..CovidVaccinations v
 	and d.date = v.date
 order by 2,3;
 
--- Use CTE
+-- Use CTE -> "percentage of population vaccinated"
 
 with PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
@@ -92,7 +107,7 @@ Join PortifolioProject..CovidVaccinations v
 select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
 from PopvsVac;
 
--- Temporary Table
+-- Temporary Table -> "percentage of population vaccinated"
 
 Drop table if exists #PercentPopulationVaccinated
 Create table #PercentPopulationVaccinated
@@ -120,7 +135,7 @@ select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
 from #PercentPopulationVaccinated;
 
 
--- Views
+-- Views -> "percentage of population vaccinated"
 
 Create View PercentPopulationVaccinated as
 select d.continent, d.location, d.date, d.population,v.new_vaccinations
